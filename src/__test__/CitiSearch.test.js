8a716ff6631696816b2d89633f3ff1d6e8720ba9
Mixtapejacob <144938167+Mixtapejacob@users.noreply.github.com>
2024-07-01 18:31:12 -0500

    msg

--- src/__test__/CitiSearch.test.js
+++ src/__test__/CitiSearch.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import CitySearch from '../components/CitySearch';
 import userEvent from '@testing-library/user-event';
 import mockData from '../mockData';
-import { extractLocations } from '../api';
+import { extractLocations, getEvents } from '../api';
 
 
 
@@ -28,4 +28,22 @@ describe('<CitySearch /> component', () => {
         const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
         expect(suggestionListItems).toHaveLength(2);
       });
+
+      test('renders the suggestion text in the textbox upon clicking on the suggestion', async () => {
+        const user = userEvent.setup();
+        const allEvents = await getEvents(); 
+        const allLocations = extractLocations(allEvents);
+        let CitySearchComponent = render(<CitySearch allLocations={allLocations} />);
+    
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.type(cityTextBox, "Berlin");
+    
+        // the suggestion's textContent look like this: "Berlin, Germany"
+        const BerlinGermanySuggestion = CitySearchComponent.queryAllByRole('listitem')[0];
+    
+        await user.click(BerlinGermanySuggestion);
+    
+        expect(cityTextBox).toHaveValue("Berlin");
+      });
+    
 });
